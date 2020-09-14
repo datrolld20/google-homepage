f7ea930807331c5e999e0795fd80897946f7c61e
datrolld20 <datrolld20@gmail.com>
2020-09-14 04:59:34 -0400

    Adding Feeling Lucky scroll functionality. Send help.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function btnVisibility() {
+function xBtnVisibility() {
     let searchBar = document.getElementById("searchbar");
     let xBtn = document.getElementById("x-btn");
 
@@ -17,3 +17,29 @@ function clearInput() {
     xBtn.style.visibility = 'hidden';
 }
 
+function resizeRandom() {
+    let feelingLucky = document.getElementById('feeling-lucky');
+    let hiddenText = document.getElementById('hidden-text');
+
+    let rand = Math.floor(Math.random() * 50);
+    let str = "";
+
+    for(let i = 0; i < rand; i++) {
+        str = str.concat('x');
+    }
+
+    hiddenText.innerHTML = str;
+    console.log(hiddenText.innerHTML);
+}
+
+function feelingWheel() {
+    let feelingLucky = document.getElementById('feeling-lucky');
+    let feelingWheel = document.getElementById('wheel-options-absolute');
+    let feelingOptions = [];
+    let hiddenText = document.getElementById('hidden-text');
+
+}
+
+function feelingReset() {
+    console.log("mousout");
+}
